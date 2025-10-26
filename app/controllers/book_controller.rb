133c87d99bc66b742489cce8420633c297d107ed
stackofsugar <digno.christopher@gmail.com>
2025-10-27 01:00:15 +0700

    Added BookController

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,25 @@
+class BookController < ApplicationController
+  include LoggingHelper
+
+  def index
+    render json: Book.select(
+      :id, :human_id, :title,
+      :isbn, :stock, :updated_at
+    ).order(:stock).order(updated_at: :desc)
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book, location: borrower_index_path
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.expect(book: [ :title, :isbn, :stock ])
+  end
+end
