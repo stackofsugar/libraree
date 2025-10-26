0620768e3a0d8b006162aa77b7d1568bc0bf5436
stackofsugar <digno.christopher@gmail.com>
2025-10-27 00:29:41 +0700

    Added SessionController for auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+  allow_unauthenticated_access only: %i[ create ]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { render status: :too_many_requests }
+
+  include LoggingHelper
+
+  def create
+    if authenticated?
+      render json: {
+        typ: "already_authenticated"
+      }
+    else
+      if user = User.authenticate_by(login_params)
+        token = start_new_session_for(user)
+        reset_session
+        render json: { token: token }
+      else
+        render json: {
+          msg: "Invalid credentials supplied",
+          typ: "invalid_credentials"
+          }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def show
+    render json: { msg: "" }
+  end
+
+  def destroy
+    terminate_session
+    render status: :ok
+  end
+
+  private
+
+  def login_params
+    params.expect(user: [ :email_address, :password ])
+  end
+end
