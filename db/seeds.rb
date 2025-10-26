dfebe2198176a5bcc79261c3a51aae3b85a74b11
stackofsugar <digno.christopher@gmail.com>
2025-10-27 00:27:00 +0700

    Added User seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[
+  "Julian McGrath", "Benjamin Parsons", "Anne Buckland", "Jane Wallace", "Kimberly Walker",
+  "Joan Baker", "Irene Parsons", "Elizabeth Edmunds", "Ella Kerr", "Leonard Wilson",
+  "Charles Black", "Jack Johnston", "Sonia Nolan", "Lillian Edmunds", "Anne Robertson",
+  "Wanda Butler", "Ian Hudson", "Sonia Sharp", "Ava Thomson", "Sophie Abraham",
+  "Wendy Davidson", "Amelia Parr", "Penelope North", "Deirdre Wright", "Joseph Young",
+  "Natalie Kelly", "Maria Cornish", "Stephen Rutherford", "Amelia Arnold", "Neil Mitchell",
+  "Leonard Oliver", "Carl Newman", "Cameron Wilkins", "Faith Turner", "Joshua MacDonald",
+  "Una Davidson", "Owen Kerr", "Jan Taylor", "Theresa Walker", "Ryan Terry",
+  "Abigail Metcalfe", "Penelope Randall", "Donna Skinner", "Yvonne Harris", "Neil Avery",
+  "Neil Manning", "Max Black", "Deirdre Rees", "Justin Campbell", "Donna Hill"
+].map.with_index do |name, index|
+  {
+    name: name,
+    email_address: name.sub(" ", "").downcase + "@example.com",
+    id_card_number: rand(10000..99999),
+    password_digest: BCrypt::Password.create(name.split.first.downcase + "Libraree123", cost: 12),
+    is_admin: (index % 10) == 0
+  }
+end.tap do |ins|
+  User.upsert_all(ins)
+end
