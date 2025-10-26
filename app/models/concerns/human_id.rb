8be677f958eb367c997543c84202f29e5093a040
stackofsugar <digno.christopher@gmail.com>
2025-10-27 00:28:35 +0700

    HumanID concert (ketinggalan)

---
+++ app/models/concerns/human_id.rb
@@ -0,0 +1,11 @@
+module HumanID
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation do
+      self.human_id = SecureRandom.base58 if new_record?
+    end
+
+    validates :human_id, presence: true
+  end
+end
