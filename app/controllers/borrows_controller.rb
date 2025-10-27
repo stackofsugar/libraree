aadfd3eca0cd3efdb4b92e0acd3de3b9256fbfbc
stackofsugar <digno.christopher@gmail.com>
2025-10-27 18:29:50 +0700

    Added features for Borrowing

---
+++ app/controllers/borrows_controller.rb
@@ -0,0 +1,70 @@
+class BorrowsController < ApplicationController
+  include LoggingHelper
+  allow_non_admin only: %i[ show create return ]
+
+  def all
+    render json: (Borrowing.includes(:book, :user).order("returned_at DESC NULLS FIRST", updated_at: :desc).map do |borrow|
+      {
+        id: borrow.id,
+        return_date: borrow.return_date,
+        returned_at: borrow.returned_at,
+        book: {
+          title: borrow.book.title,
+        },
+        user: {
+          name: borrow.user.name,
+          email_address: borrow.user.email_address,
+        },
+        updated_at: borrow.updated_at,
+      }
+    end)
+  end
+
+  def show
+    render json: (current_user.borrowing.where(returned_at: nil).first.then do |borrow|
+      borrow ? {
+        id: borrow.id,
+        return_date: borrow.return_date,
+        book_title: borrow.book.title,
+        updated_at: borrow.updated_at
+      } : {}
+    end)
+  end
+
+  def create
+    if current_user.borrowing.where(returned_at: nil).present?
+      render json: {
+        base: [ "You have an active loan. Return your book before attempting to borrow more" ]
+      }, status: :unprocessable_entity
+    else
+      @borrowing = Borrowing.new(borrowing_params.merge({ user_id: current_user.id }))
+      @borrowing.borrow = true
+      if @borrowing.save
+        render json: {
+          id: @borrowing.id,
+          return_date: @borrowing.return_date,
+          book_title: @borrowing.book.title
+        }
+      else
+        render json: @borrowing.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def return
+    if current_user.borrowing.where(returned_at: nil).present?
+      current_user.borrowing.where(returned_at: nil).first.update(returned_at: Date.current)
+      render json: {}
+    else
+      render json: {
+        base: [ "You have no outstanding loans" ]
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def borrowing_params
+    params.expect(borrowing: [ :return_date, :book_id ])
+  end
+end
--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,10 +1,29 @@
 class Borrowing < ApplicationRecord
   include HumanId
 
+  attr_accessor :borrow
+
   belongs_to :book
   belongs_to :user
 
-  validates_associated :book, :user
-  validates :book, :user, presence: true
+  before_validation :decrement_book_stock, if: :borrow
+  before_validation :increment_book_stock, unless: :borrow
+
+  validates_associated :book, message: "has no stock"
   validates :return_date, comparison: { greater_than: Date.current, message: "has already passed" }
+  validate :borrow_duration
+
+  def borrow_duration
+    if (self.return_date - Date.current).to_i > 30
+      errors.add(:return_date, "is too long (can't borrow for more than 30 days)")
+    end
+  end
+
+  def decrement_book_stock
+    self.book&.decrement!(:stock)
+  end
+
+  def increment_book_stock
+    self.book.increment!(:stock)
+  end
 end
