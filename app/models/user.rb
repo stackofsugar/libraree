9dd7395d3d90e5762ba2a6fb500e284ba616ba37
stackofsugar <digno.christopher@gmail.com>
2025-10-27 18:24:26 +0700

    Change User->Borrowing to has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-  has_one :borrowing, dependent: :destroy
+  has_many :borrowing, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   normalizes :name, with: ->(e) { e.strip.titlecase }
