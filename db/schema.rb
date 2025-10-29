6a888756909106b174ff703e9805d38a4ca8e549
stackofsugar <digno.christopher@gmail.com>
2025-10-29 12:41:00 +0700

    Move to SQLite

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_10_24_112147) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "books", force: :cascade do |t|
     t.string "title", limit: 300, null: false
     t.string "isbn", limit: 17, null: false
